8a8e1f739b593626f6218b92428f8c7db148bc5f
Chuck476 <cdierbach@towson.edu>
2024-01-26 19:41:48 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,33 @@
+// Practicum 13 (while and for loops)
+// (TO COMPLETE)
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // var declarations
+        int n, sum1, sum2;
+     
+        Scanner input = new Scanner(System.in);
+
+        // prompt user for value of n to sum up to
+        // if then enter 5, should add up 1 to 5 (equal to 15)
+       
+
+        // use a for loop to sum up the values and assign to sum1
+
+
+        // use a while loop to sum up the values and assign to sum2
+        
+
+        // display results
+        System.out.println(
+          "Sum of 1.." + n + " by use of for loop is " + sum1);
+
+        System.out.println(
+          "Sum of 1.." + n + " by use of while loop is " + sum2);
+    }
+}
